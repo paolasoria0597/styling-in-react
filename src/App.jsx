9b9f62a9e32275f40398fbbeef9290f2f707235b
paolasoria <paola.soria0597@gmail.com>
2024-10-17 09:45:11 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+// Later on, we'll import button components here
+
+const App = () => {
+  return (
+    <div className="App">
+      {
+        // Add all example components:
+        <Button1 buttonText="Example 1" />
+        //...
+      }
+    </div>
+  );
+};
+
+export default App;
+
